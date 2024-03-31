7f57739da77b7e673853fb3d7572156811c1cdd6
Hannalee Wu <hannaleehzw@gmail.com>
2024-03-30 22:19:32 -0400

    hi

--- imageRec/imageRec/urls.py
+++ imageRec/imageRec/urls.py
@@ -25,6 +25,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include("polls.urls")),
+    path('', include('polls.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
